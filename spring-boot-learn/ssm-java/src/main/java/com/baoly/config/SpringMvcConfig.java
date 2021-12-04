23f46c429ea44ad41e991341238cf6e36a025dd7
baoly <nightcatbaoly@163.com>
2021-12-04 15:50:26 +0800

    配置静态资源映射 配置视图解析器

--- spring-boot-learn/ssm-java/src/main/java/com/baoly/config/SpringMvcConfig.java
+++ spring-boot-learn/ssm-java/src/main/java/com/baoly/config/SpringMvcConfig.java
@@ -4,11 +4,29 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @ComponentScan(basePackages = {"com.baoly"}, useDefaultFilters = false,
         includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Controller.class),
                 @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         })
 @Configuration
-public class SpringMvcConfig {
+public class SpringMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/");
+    }
+
+    @Override
+    protected void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/hello1").setViewName("index");
+    }
+
+    @Override
+    protected void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("jsp/", ".jsp");
+    }
 }
---
+++ spring-boot-learn/ssm-java/src/main/webapp/jsp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Lenovo
+  Date: 2021/12/4
+  Time: 15:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+hello jsp
+</body>
+</html>
